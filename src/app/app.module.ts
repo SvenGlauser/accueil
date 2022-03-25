9a21811c9f19f8754ce9727f0b8b266d253b398e
Sven Glauser <sven.glauser@divtec.ch>
2022-03-25 18:27:15 +0100

    Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
+import { AccueilComponent } from './page/accueil/accueil.component';
+import { HeaderComponent } from './page/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
