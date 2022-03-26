eae5a8418876975e96b7717ba1c050ed8cb81170
Sven Glauser <sven.glauser@divtec.ch>
2022-03-26 18:24:34 +0100

    Modifier les projets

--- src/app/data/projets.ts
+++ src/app/data/projets.ts
@@ -5,7 +5,7 @@ export const PROJETS: Repository[] = [
     name: "Todo list Backend",
     description: `
         <p>Développement d'une API pour le backend d'une application permettant de gérer des tâches → todo list</p>
-        <p>Utilisation des technologies suivantes</p>
+        <p>Utilisation des technologies suivantes :</p>
         <ul>
           <li>Framework Java Spring</li>
           <li>Base de données PostgreSQL</li>
@@ -27,12 +27,18 @@ export const PROJETS: Repository[] = [
     name: "Application interne",
     description: `
         <p>Développement d'une API pour le backend d'une application permettant le remplissage d'un formulaire</p>
-        <p>Utilisation des technologies suivantes</p>
+        <p>Utilisation des technologies suivantes :</p>
         <ul>
           <li>Framework Java Spring</li>
           <li>Base de données PostgreSQL</li>
           <li>Liquibase pour créer la DB avec Spring</li>
           <li>Base de donnée virtualisée avec Docker</li>
+        </ul>
+        <p>Développement du front avec une connection au Backend</p>
+        <p>Utilisation des technologies suivantes :</p>
+        <ul>
+          <li>Angular</li>
+          <li>Keycloak</li>
         </ul>
       `,
     img: "assets/github/form.png",
