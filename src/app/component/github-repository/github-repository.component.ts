171b102a639e2c8e19926158c056ff397874d735
Sven Glauser <sven.glauser@divtec.ch>
2022-03-26 13:51:19 +0100

    Changer le theme.scss

--- src/app/component/github-repository/github-repository.component.ts
+++ src/app/component/github-repository/github-repository.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-github-repository',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-repository.component.css']
 })
 export class GithubRepositoryComponent implements OnInit {
+
+  @Input() repository: string | undefined
 
   constructor() { }
 
