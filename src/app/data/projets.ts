423ca15e6f72eae2a5ba974ce17e4e1ff00cef24
Sven Glauser <sven.glauser@divtec.ch>
2022-06-25 16:04:08 +0200

    Modifier les projets

--- src/app/data/projets.ts
+++ src/app/data/projets.ts
@@ -41,34 +41,33 @@ export const PROJETS: Repository[] = [
     url: "https://github.com/SvenGlauser/accueil"
   },
   {
-    name: "Application interne",
+    name: "The Movies Informations",
     description: `
-        <p>Développement d'une API pour le backend d'une application permettant le remplissage d'un formulaire</p>
-        <p>Utilisation des technologies suivantes :</p>
-        <ul>
-          <li>Framework Java Spring</li>
-          <li>Base de données PostgreSQL</li>
-          <li>Liquibase pour créer la DB avec Spring</li>
-          <li>Base de donnée virtualisée avec Docker</li>
-        </ul>
-        <p>Développement du front avec une connection au Backend</p>
+        <p>Développement d'un site web connecté à l'API de The Movie Database qui récupère les infos des acteurs, des films et des séries et les affiche à l'utilisateur</p>
         <p>Utilisation des technologies suivantes :</p>
         <ul>
           <li>Angular</li>
-          <li>Keycloak</li>
         </ul>
-      `,
-    img: "assets/github/form.png",
-    imgAlt: "Icône formulaire",
-    technologie: [
-      "Spring Boot",
-      "Spring Security",
-      "Spring JPA",
-      "Liquibase",
-      "PostgreSQL",
-      "Angular",
-      "Keycloak"
-    ],
-    url: ""
+    `,
+    img: "https://glausve.divtec.me/movies/favicon.ico",
+    imgAlt: "Icône films",
+    technologie: ["Angular"],
+    url: "https://github.com/SvenGlauser/movies-angular"
+  },
+  {
+    name: "Horloge digitale",
+    description: `
+        <p>Horloge digitale</p>
+        <p>Utilisation des technologies suivantes :</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+    `,
+    img: "https://github.com/favicon.ico",
+    imgAlt: "Icône films",
+    technologie: ["HTML", "CSS", "JavaScript"],
+    url: "https://github.com/SvenGlauser/clock"
   }
 ]
